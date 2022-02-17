1c8865ff204021a7b6bb26dc72e0e2707aaa9d2a
Marc Lelarge <marc.lelarge@gmail.com>
2022-02-17 09:39:41 +0100

    added moco for CIFAR10

---
+++ moco/loader_mini.py
@@ -0,0 +1,30 @@
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+from PIL import Image
+from torch.utils.data import DataLoader
+
+class CIFAR10Pair(CIFAR10):
+    """CIFAR10 Dataset.
+    """
+    def __getitem__(self, index):
+        img = self.data[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            im_1 = self.transform(img)
+            im_2 = self.transform(img)
+
+        return im_1, im_2
+
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(32),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
+
